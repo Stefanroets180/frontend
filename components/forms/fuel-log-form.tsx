61ecc0661c47fc5b7b01b1fd1f61605545eb92b3
stefanroetsjetcode-code <61413955+Stefanroets180@users.noreply.github.com>
2026-08-08 23:34:01 +0200

    Add form state debug logging to check validation errors

--- components/forms/fuel-log-form.tsx
+++ components/forms/fuel-log-form.tsx
@@ -154,7 +154,7 @@ export function FuelLogForm({ vehicles, onSubmit, initialData, mode = 'create', 
     handleSubmit,
     watch,
     setValue,
-    formState: { errors },
+    formState: { errors, isValid, isDirty },
   } = useForm<FuelLogInput>({
     resolver: zodResolver(fuelLogSchema),
     defaultValues: initialData || {
@@ -165,6 +165,11 @@ export function FuelLogForm({ vehicles, onSubmit, initialData, mode = 'create', 
       odometerReading: vehicles[0]?.currentOdometer || 0,
     },
   })
+
+  // Debug: Log form state
+  useEffect(() => {
+    console.log('Form state:', { isValid, isDirty, errors, mode })
+  }, [isValid, isDirty, errors, mode])
 
   const selectedVehicleId = watch('vehicleId')
   const liters = watch('liters')
