6bfab9c4745dc32d7dbc8b82d7613b2cd9fbe751
stefanroetsjetcode-code <61413955+Stefanroets180@users.noreply.github.com>
2026-08-08 23:28:19 +0200

    Add debug logging to investigate save issue after GPS clear

--- components/forms/fuel-log-form.tsx
+++ components/forms/fuel-log-form.tsx
@@ -314,13 +314,16 @@ export function FuelLogForm({ vehicles, onSubmit, initialData, mode = 'create', 
   }
 
   const handleFormSubmit = async (data: FuelLogInput) => {
+    console.log('handleFormSubmit called', { data, gpsPosition, mode })
     setIsSubmitting(true)
     try {
+      console.log('About to call onSubmit')
       await onSubmit(
         data,
         mode === 'create' ? receiptImage || undefined : undefined,
         gpsPosition || undefined
       )
+      console.log('onSubmit completed successfully')
       
       // Save fuel log odometer to trigger tyre rotation check
       // This will update any active tyre rotation tracking for this vehicle
