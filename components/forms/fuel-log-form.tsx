d3a4ac70f947041b79c41cce425b0752550b7e2d
stefanroetsjetcode-code <61413955+Stefanroets180@users.noreply.github.com>
2026-09-15 15:36:24 +0200

    Fix accessibility issues in fuel-log-form by adding name attributes to remaining Input fields

--- components/forms/fuel-log-form.tsx
+++ components/forms/fuel-log-form.tsx
@@ -479,6 +479,7 @@ export function FuelLogForm({ vehicles, onSubmit, initialData, mode = 'create', 
               <Label htmlFor="liters">Liters</Label>
               <Input
                 id="liters"
+                name="liters"
                 {...register('liters')}
                 type="number"
                 inputMode="decimal"
@@ -494,6 +495,7 @@ export function FuelLogForm({ vehicles, onSubmit, initialData, mode = 'create', 
               <Label htmlFor="pricePerLiter">Price/L (R)</Label>
               <Input
                 id="pricePerLiter"
+                name="pricePerLiter"
                 {...register('pricePerLiter')}
                 type="number"
                 inputMode="decimal"
@@ -522,6 +524,7 @@ export function FuelLogForm({ vehicles, onSubmit, initialData, mode = 'create', 
             <Label htmlFor="odometerReading">Odometer (km)</Label>
             <Input
               id="odometerReading"
+              name="odometerReading"
               {...register('odometerReading', { valueAsNumber: true })}
               type="number"
               inputMode="numeric"
@@ -569,6 +572,7 @@ export function FuelLogForm({ vehicles, onSubmit, initialData, mode = 'create', 
             <Label htmlFor="stationName">Station Name</Label>
             <Input
               id="stationName"
+              name="stationName"
               {...register('stationName')}
               placeholder="e.g., Shell, Engen, Sasol"
               className="h-12 touch-target"
@@ -578,6 +582,7 @@ export function FuelLogForm({ vehicles, onSubmit, initialData, mode = 'create', 
             <Label htmlFor="stationLocation">Location</Label>
             <Input
               id="stationLocation"
+              name="stationLocation"
               {...register('stationLocation')}
               placeholder="e.g., N1 Highway, Johannesburg"
               className="h-12 touch-target"
