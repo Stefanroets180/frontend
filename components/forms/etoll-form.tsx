013159a624314a211bbf04640cc5e062d9decdb6
stefanroetsjetcode-code <61413955+Stefanroets180@users.noreply.github.com>
2026-09-15 16:16:12 +0200

    Add name attributes to period date inputs in etoll-form

--- components/forms/etoll-form.tsx
+++ components/forms/etoll-form.tsx
@@ -371,6 +371,7 @@ export function ETollForm({
               <Label htmlFor="periodStartDate">Period Start (Optional)</Label>
               <Input
                 id="periodStartDate"
+                name="periodStartDate"
                 type="text"
                 inputMode="numeric"
                 pattern="\d{4}-\d{2}-\d{2}"
@@ -423,6 +424,7 @@ export function ETollForm({
               <Label htmlFor="periodEndDate">Period End (Optional)</Label>
               <Input
                 id="periodEndDate"
+                name="periodEndDate"
                 type="text"
                 inputMode="numeric"
                 pattern="\d{4}-\d{2}-\d{2}"
